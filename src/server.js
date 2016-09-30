8c409752a3e8bde7740c1db5ae7af3c78435fccd
Jimmy Letecheur <jimmy@letecheur.me>
2016-09-30 12:22:56 +0200

    Basic setup of express

--- src/server.js
+++ src/server.js
@@ -1,3 +1,30 @@
 "user strict";
 
 console.log( "kikoo" );
+
+import express from "express";
+import responseTime from "response-time";
+import bodyParser from "body-parser";
+
+const APP_PORT = 8080;
+let oApp;
+
+// configure express
+oApp = express();
+
+// configure middleware
+oApp.use( responseTime() );
+oApp.use( bodyParser.json() );
+oApp.use( bodyParser.urlencoded({
+    "extended": true,
+} ) );
+
+// configure base temporary route
+oApp.get( "/", ( oRequest, oResponse ) => {
+    oResponse.send( "Hello, world!" );
+} );
+
+// listening
+oApp.listen( APP_PORT, () => {
+    console.log( `Server is listening on port 8080 ${ APP_PORT }` );
+} );
