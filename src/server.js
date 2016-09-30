1f0f5e378832a429f6dddfb88b1c7a85cf24daa5
QuentinGeorge <quentingeorge7@gmail.com>
2016-09-30 12:23:07 +0200

    Basic setup of express

--- src/server.js
+++ src/server.js
@@ -6,6 +6,30 @@
  * started at 30/09/2016
  */
 
-"use strict";
+import express from "express";
+import responseTime from "response-time";
+import bodyParser from "body-parser";
 
-console.log( "Coucou" );
+const APP_PORT = 8080;
+
+let oApp;
+
+// configure express
+oApp = express();
+
+// configure middleware
+oApp.use( responseTime() );
+oApp.use( bodyParser.json() );
+oApp.use( bodyParser.urlencoded( {
+    "extended": true,
+} ) );
+
+// configure base temporary route
+oApp.get( "/", ( oRequest, oResponse ) => {
+    oResponse.send( "Hello, world!" );
+} );
+
+// listening
+oApp.listen( APP_PORT, () => {
+    console.log( `Server is listeninng on port ${ APP_PORT }` ); // eslint-disable-line no-console
+} );
