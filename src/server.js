5c34fb3efcb644bbc7e55626fc0741c01cda7430
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-09-30 12:23:15 +0200

    Basic setup of express

--- src/server.js
+++ src/server.js
@@ -1,4 +1,23 @@
 
 
+import express from "express";
+import responseTime from "response-time";
+import bodyParser from "body-parser";
+const APP_PORT = 8080;
+let oApp;
 
-console.log( "kikoo" );
+oApp = express();
+
+oApp.use( responseTime() );
+oApp.use( bodyParser.json() );
+oApp.use( bodyParser.urlencoded( {
+    "extended": true,
+} ) );
+
+oApp.get( "/", ( oRequest, oResponse ) => {
+    oResponse.send( "Hello, world!" );
+} );
+
+oApp.listen( APP_PORT, () => {
+    console.log( `Server is listening  on port ${ APP_PORT }` ); // eslint-disable-line no-console
+} );
