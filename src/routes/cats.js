5a94c7fd22ea58e61e3fd5687e7a5acd8f41b93d
leny <info@flatland.be>
2016-10-07 12:18:29 +0200

    Implement cats add controller

--- src/routes/cats.js
+++ src/routes/cats.js
@@ -9,12 +9,13 @@
 import { Router } from "express";
 
 import list from "../controllers/cats/list";
+import add from "../controllers/cats/add";
 
 let oRouter = new Router();
 
 oRouter.get( "/cats", list );
 // oRouter.get( "/cats/:name", details );
-// oRouter.post( "/cats", add );
+oRouter.post( "/cats", add );
 // oRouter.put( "/cats/:name", update );
 // oRouter.delete( "/cats/:name", destroy );
 
