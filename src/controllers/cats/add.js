0e3336cc577aa73626c4a7e029d9462eef048161
Mathieu Claessens <mathieu_claessens@hotmail.com>
2016-10-07 12:19:13 +0200

    Implement Cats add controller

---
+++ src/controllers/cats/add.js
@@ -0,0 +1,66 @@
+/* ria/prevkonsep
+ *
+ * /src/controllers/cats/add.js - API Controller for cats adding
+ *
+ * Coded by Mucht - Mathieu Claessens
+ * started at 07/10/2016
+*/
+
+import { db } from "../../core/mongodb";
+
+const GENDERS = [ "male", "female" ];
+
+export default function( oRequest, oResponse ) {
+    const POST = oRequest.body;
+
+    let sName = ( POST.name || "" ).trim(),
+        iAge = +POST.age, // = parseInt( POST.age, 10 )
+        sGender = POST.gender,
+        sColor = ( POST.color || "" ).trim(),
+        aErrors = [],
+        oCat;
+
+    // Error managment
+        // Name
+    if ( !sName ) {
+        aErrors.push( "name can't be empty!" );
+    }
+        // Age
+    if ( isNaN( iAge ) ) {
+        aErrors.push( "age isn't a number!" );
+    }
+        // Gender
+    if ( GENDERS.indexOf( sGender ) === -1 ) {
+        aErrors.push( `invalid gender: must be "male" or "female"!` );
+    }
+        // Color
+    if ( !sColor ) {
+        aErrors.push( "color can't be empty!" );
+    }
+
+    if ( aErrors.length ) {
+        return oResponse.status( 400 ).json( {
+            "errors": aErrors,
+        } );
+    }
+
+    oCat = {
+        "name": sName,
+        "age": Math.abs( iAge ),
+        "gender": sGender,
+        "color": sColor,
+        "create": new Date(),
+        "update": new Date(),
+    };
+
+    db.collection( "cats" )
+        .insertOne( oCat )
+            .then( () => {
+                oResponse.status( 201 ).json( oCat )
+            } )
+            .catch( ( oError ) => {
+                oResponse.status( 500 ).json( {
+                    "errors": [ oError ],
+                } );
+            } );
+}
