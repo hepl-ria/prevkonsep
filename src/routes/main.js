e262b6d6c5328a3f9e09c4e7471af0ae07af5044
Mathieu Claessens <mathieu_claessens@hotmail.com>
2016-10-07 11:14:29 +0200

    Extract routes from express.js

---
+++ src/routes/main.js
@@ -0,0 +1,15 @@
+/* ria/prevkonsep
+ *
+ * /src/routes/main.js - Main routes
+ *
+ * Coded by Mucht - Mathieu Claessens
+ * started at 07/10/2016
+*/
+
+import { Router } from "express";
+
+let oRouter = new Router();
+
+oRouter.get( "/", ( oRequest, oResponse ) => {
+    oResponse.send( "Hi! This is a REST cats API!" );
+} );
