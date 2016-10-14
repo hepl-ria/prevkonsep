ea7b471d8709dfc2cb25ace5f81cba1e5c7fda24
Mathieu Claessens <mathieu_claessens@hotmail.com>
2016-10-14 16:07:49 +0200

    Add controller to get a dog's details

--- src/routes/dogs.js
+++ src/routes/dogs.js
@@ -9,7 +9,7 @@
 import { Router } from "express";
 import list from "../controllers/dogs/list";
 import add from "../controllers/dogs/add";
-// import details from "../controllers/dogs/details";
+import details from "../controllers/dogs/details";
 // import destroy from "../controllers/dogs/destroy";
 // import update from "../controllers/dogs/update";
 
@@ -17,7 +17,7 @@ let oRouter = new Router();
 
 oRouter.get( "/dogs", list );
 oRouter.post( "/dogs", add );
-// oRouter.get( "/dogs/:slug", details );
+oRouter.get( "/dogs/:slug", details );
 // oRouter.delete( "/dogs/:slug", destroy );
 // oRouter.put( "/dogs/:slug", update );
 
