f5520539801c0fb12776fea83eeb06f2b22590d7
leny <info@flatland.be>
2016-10-14 11:54:27 +0200

    Add controller for cat edition

--- src/routes/cats.js
+++ src/routes/cats.js
@@ -11,6 +11,7 @@ import { Router } from "express";
 import list from "../controllers/cats/list";
 import add from "../controllers/cats/add";
 import details from "../controllers/cats/details";
+import update from "../controllers/cats/update";
 import destroy from "../controllers/cats/destroy";
 
 let oRouter = new Router();
@@ -18,7 +19,7 @@ let oRouter = new Router();
 oRouter.get( "/cats", list );
 oRouter.get( "/cats/:slug", details );
 oRouter.post( "/cats", add );
-// oRouter.put( "/cats/:slug", update );
+oRouter.put( "/cats/:slug", update );
 oRouter.delete( "/cats/:slug", destroy );
 
 export default oRouter;
