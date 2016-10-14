b2a852aae47c353797749cbbab3956f437a8ef39
Pauline Viroux <pilineviroux@hotmail.com>
2016-10-14 10:02:45 +0200

    Avoid duplication of cats in insertion

--- src/controllers/cats/add.js
+++ src/controllers/cats/add.js
@@ -1,8 +1,11 @@
-/*
-*
-* /src/controllers/add.js - API controller to add cats
-*
+/* ria/prevkonsep
+ *
+ * /src/controllers/cats/add.js - API Controller for cats adding
+ *
+ * Coded by - Paulineviroux
+ * started at 07/10/2016
 */
+
 import { db } from "../../core/mongodb";
 
 const GENDERS = [ "male", "female" ];
@@ -11,52 +14,70 @@ export default function( oRequest, oResponse ) {
     const POST = oRequest.body;
 
     let sName = ( POST.name || "" ).trim(),
-        iAge = +POST.age,
+        iAge = +POST.age, // = parseInt( POST.age, 10 )
         sGender = POST.gender,
         sColor = ( POST.color || "" ).trim(),
         aErrors = [],
         oCat;
 
+    // Error managment
+        // Name
     if ( !sName ) {
-        aErrors.push( "name can't be empty" );
+        aErrors.push( "name can't be empty!" );
     }
-
+        // Age
     if ( isNaN( iAge ) ) {
         aErrors.push( "age isn't a number!" );
     }
-
-    if ( GENDERS.indexOf( sGender ) === -1 ) { //le genre qu'on nous a donné, est ce qu'il est present de le tableau de mes genres 
+        // Gender
+    if ( GENDERS.indexOf( sGender ) === -1 ) {
         aErrors.push( `invalid gender: must be "male" or "female"!` );
     }
-
+        // Color
     if ( !sColor ) {
-        aErrors.push( "color can't be empty" );
+        aErrors.push( "color can't be empty!" );
     }
 
     if ( aErrors.length ) {
         return oResponse.status( 400 ).json( {
             "errors": aErrors,
-        } )
+        } );
     }
 
-    oCat = {
-        "name": sName,
-        "age": Math.abs( iAge ),
-        "gender": sGender,
-        "color": sColor,
-        "create": new Date(),
-        "update": new Date(),
-    };
-
     db.collection( "cats" )
-        .insertOne( oCat )
-            .then( () => { //appelée si l'objet a été créé donc on l'appelle!
-                oResponse.status( 201 ).json( oCat );
-            } )
-            .catch( ( oError ) => {
-                oResponse.status( 500 ).json( {
-                    "errors": [ oError ],
-                } )
-            });
+        .findOne( {
+            "name": sName,
+        } )
+        .then( ( oCatFromDB ) => {
+            if ( oCatFromDB ) {
+                return oResponse.status( 409 ).json( {
+                    "errors": [ `A cat with the name "${ sName }" already exists!` ],
+                } );
+            }
 
+            oCat = {
+                "name": sName,
+                "age": Math.abs( iAge ),
+                "gender": sGender,
+                "color": sColor,
+                "create": new Date(),
+                "update": new Date(),
+            };
+
+            db.collection( "cats" )
+                .insertOne( oCat )
+                    .then( () => {
+                        oResponse.status( 201 ).json( oCat );
+                    } )
+                    .catch( ( oError ) => {
+                        oResponse.status( 500 ).json( {
+                            "errors": [ oError ],
+                        } );
+                    } );
+        } )
+        .catch( ( oError ) => {
+            oResponse.status( 500 ).json( {
+                "errors": [ oError ],
+            } );
+        } );
 }
