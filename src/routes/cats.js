3b986d3a1904207b9103f3b403d9957da03f1959
Jimmy Letecheur <jimmy@letecheur.me>
2016-10-14 11:18:29 +0200

    Default value for slugify utils

--- src/routes/cats.js
+++ src/routes/cats.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import list from "../controllers/cats/list";
 import add from "../controllers/cats/add";
 import details from "../controllers/cats/details";
+import destroy from "../controllers/cats/destroy";
 
 let oRouter = new Router();
 
@@ -9,6 +10,6 @@ oRouter.get( "/cats", list );
 oRouter.get( "/cats/:slug", details );
 oRouter.post( "/cats", add );
 // oRouter.put( "/cats/:slug", update );
-// oRouter.delete( "/cats/:slug", destroy );
+oRouter.delete( "/cats/:slug", destroy );
 
 export default oRouter;
