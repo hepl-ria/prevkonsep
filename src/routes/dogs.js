d32cb94cd216be8b615dcef85569a0a5efe5f7a3
Mathieu Claessens <mathieu_claessens@hotmail.com>
2016-10-14 16:31:04 +0200

    Add controller to delete a dog

--- src/routes/dogs.js
+++ src/routes/dogs.js
@@ -10,7 +10,7 @@ import { Router } from "express";
 import list from "../controllers/dogs/list";
 import add from "../controllers/dogs/add";
 import details from "../controllers/dogs/details";
-// import destroy from "../controllers/dogs/destroy";
+import destroy from "../controllers/dogs/destroy";
 // import update from "../controllers/dogs/update";
 
 let oRouter = new Router();
@@ -18,7 +18,7 @@ let oRouter = new Router();
 oRouter.get( "/dogs", list );
 oRouter.post( "/dogs", add );
 oRouter.get( "/dogs/:slug", details );
-// oRouter.delete( "/dogs/:slug", destroy );
+oRouter.delete( "/dogs/:slug", destroy );
 // oRouter.put( "/dogs/:slug", update );
 
 export default oRouter;
