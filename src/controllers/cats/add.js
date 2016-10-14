39472a4589fc9843b6d18274fb7c4817013faea3
leny <info@flatland.be>
2016-10-14 12:01:29 +0200

    Fix route error catching

--- src/controllers/cats/add.js
+++ src/controllers/cats/add.js
@@ -74,13 +74,13 @@ export default function( oRequest, oResponse ) {
                     } )
                     .catch( ( oError ) => {
                         oResponse.status( 500 ).json( {
-                            "errors": [ oError ],
+                            "errors": [ oError.toString() ],
                         } );
                     } );
         } )
         .catch( ( oError ) => {
             oResponse.status( 500 ).json( {
-                "errors": [ oError ],
+                "errors": [ oError.toString() ],
             } );
         } );
 }
