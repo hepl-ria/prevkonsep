b4e4aad0c7b4c1f3fd7990e2963f25c889634fb6
Anne Remacle <anne.m.remacle@gmail.com>
2016-10-20 16:41:56 +0200

    Controller to get details about pandas

---
+++ src/controllers/red-pandas/details.js
@@ -0,0 +1,29 @@
+/* prevkonsep
+ *
+ * /src/controllers/details.js - Controllers
+ *
+ * coded by Anne
+ * started at 14/10/2016
+ */
+
+ import { db } from "../../core/mongodb";
+ import { slugify } from "../../core/utils";
+
+ export default function( oRequest, oResponse ) {
+
+ 	db.collection( "red-pandas" )
+ 		.findOne( {
+ 			"slug": slugify( oRequest.params.slug ),
+ 		} )
+ 		.then( ( oPanda ) => {
+ 			if( oPanda ) {
+ 				return oResponse.json( oPanda );
+ 			}
+ 			oResponse.sendStatus( 404 );
+ 		} )
+ 		.catch( ( oError ) => {
+ 			oResponse.status( 500 ).json( {
+ 				"errors": [ oError.toString() ],
+ 			} );
+ 		} )
+ }
