ad3b6e22e0b53f3a4e7ba4783e6d5bcbc30dc655
Anne Remacle <anne.m.remacle@gmail.com>
2016-10-20 16:21:34 +0200

    Editing db file

--- src/core/mongodb.js
+++ src/core/mongodb.js
@@ -12,7 +12,7 @@ import Promise from "bluebird";
 import zouti from "zouti";
 
 // promise: nouveau type de donnée censé être natif dans ES2015, mais en attendant que tout soit bien implémenté on utilise les primise de bluebird
-const MONGO_URL = "mongodb://127.0.0.1:27017/cats";
+const MONGO_URL = "mongodb://127.0.0.1:27017/animals";
 
 let oDB;
 
