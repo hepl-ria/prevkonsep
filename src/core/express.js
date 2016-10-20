2c0396e00727b0b5e51b53f95c6e8d1198d9b1be
Jimmy Letecheur <jimmy@letecheur.me>
2016-10-20 22:59:19 +0200

    update express.js

--- src/core/express.js
+++ src/core/express.js
@@ -1,32 +1,23 @@
-/* leny/prevkonsep
- *
- * /src/core/express.js - Main entry point
- *
- * coded by leny@flatLand!
- * started at 07/10/2016
- */
-
 import express from "express";
 import responseTime from "response-time";
 import bodyParser from "body-parser";
 import zouti from "zouti";
-import mainRoutes from "../routes/main";
 import simpleLog from "./middlewares/log";
-import catsRoutes from "../routes/cats";
+import mainRoutes from "../routes/main";
+import catsRoutes from "../routes/parrots";
+import parrotsRoutes from "../routes/parrots";
 
 const APP_PORT = 8080;
 
 let oApp;
 
-export default function( iAppPort = APP_PORT ) { // Pouvoir utiliser express dans autre part qu'ici
-    if( oApp ) {
+export default function( iAppPort = APP_PORT ) {
+
+    if ( oApp ) {
         return oApp;
-    } // Si la fonction oApp existe déjà, on s'arrête la
+    }
 
-    // configure express
     oApp = express();
-
-// configure middleware
     oApp.use( simpleLog );
     oApp.use( responseTime() );
     oApp.use( bodyParser.json() );
@@ -34,14 +25,11 @@ export default function( iAppPort = APP_PORT ) { // Pouvoir utiliser express dan
         "extended": true,
     } ) );
 
-// configure base temporary route
     oApp.use( mainRoutes );
     oApp.use( catsRoutes );
+    oApp.use( parrotsRoutes );
 
-// listening
     oApp.listen( iAppPort, () => {
-        zouti.log( `Server is listening on port ${ iAppPort }` ); // eslint-disable-line no-console
+        zouti.log( `Server is listening  on port ${ iAppPort }`, "jimmy/prevkonsep" );
     } );
-
 }
-
