e34c2d552343f0e6824b1f0e58cf42d72bfea208
Anne Remacle <anne.m.remacle@gmail.com>
2016-10-20 16:22:03 +0200

    Routes for red pandas

---
+++ src/routes/red-pandas.js
@@ -0,0 +1,24 @@
+/* prevkonsep
+ *
+ * /src/routes/red-pandas.js - REST red pandas routes
+ *
+ * coded by Anne
+ * started at 18/10/2016
+ */
+
+import { Router } from "express";
+import list from "../controllers/red-pandas/list";
+//import add from "../controllers/red-pandas/add";
+//import details from "../controllers/red-pandas/details";
+//import destroy from "../controllers/red-pandas/destroy";
+//import update from "../controllers/red-pandas/update";
+
+let oRouter = new Router();
+
+oRouter.get( "/red-pandas", list );
+//oRouter.get( "/red-pandas/:slug", details );
+//oRouter.post( "/red-pandas", add );
+//oRouter.put( "/red-pandas/:slug", update );
+//oRouter.delete( "/red-pandas/:slug", destroy );
+
+export default oRouter;
