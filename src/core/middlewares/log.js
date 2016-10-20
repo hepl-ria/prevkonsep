dd3698d460c3793cc2cac72689dd2cf642b24686
Jimmy Letecheur <jimmy@letecheur.me>
2016-10-20 22:57:59 +0200

    update middlewares/logs.js

--- src/core/middlewares/log.js
+++ src/core/middlewares/log.js
@@ -1,14 +1,6 @@
-/* leny/prevkonsep
- *
- * /src/core/middlewares/log.js - Simple log middleware for express
- *
- * coded by leny@flatLand!
- * started at 30/09/2016
- */
-
 import zouti from "zouti";
 
 export default function( { method, url }, oResponse, fNext ) {
-    zouti.log( `${ method } ${ url }` ); // eslint-disable-line no-console
+    zouti.log( `${ method } ${ url }`, "jimmy/prevkonsep" );
     fNext();
 }
