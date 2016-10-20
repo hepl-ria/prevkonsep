dedd5c340f5e8332abb62b7e59d7b1aa0946bc65
Jimmy Letecheur <jimmy@letecheur.me>
2016-10-20 22:58:43 +0200

    create controllers /parrots/destroy.js

--- src/controllers/cats/destroy.js
+++ src/controllers/cats/destroy.js
@@ -1,38 +1,45 @@
-import { db } from "../../core/mongodb"; // importer database
+/* leny/prevkonsep
+ *
+ * /src/controllers/cats/destroy.js - Controllers for Cats deletion
+ *
+ * coded by leny@flatLand!
+ * started at 14/10/2016
+ */
+
+import { db } from "../../core/mongodb";
 import { slugify } from "../../core/utils";
 
-export default function ( oRequest, oResponse ) {
-    db.collection("cats")
-        .findOne({
-            "slug": slugify( oRequest.params.slug),
-        })
-        .then((oCat) => {
-            if( !oCat ){
+export default function( oRequest, oResponse ) {
+    db.collection( "cats" )
+        .findOne( {
+            "slug": slugify( oRequest.params.slug ),
+        } )
+        .then( ( oCat ) => {
+            if ( !oCat ) {
                 return oResponse.sendStatus( 404 );
             }
 
             db.collection( "cats" )
-                .deleteOne({
+                .deleteOne( {
                     "_id": oCat._id,
-                })
+                } )
                 .then( ( { deletedCount } ) => {
-                    if ( deletedCount === 1 ){
+                    if ( deletedCount === 1 ) {
                         return oResponse.sendStatus( 204 );
                     }
-                    return oResponse.status( 500 ).json( {
+                    oResponse.status( 500 ).json( {
                         "errors": [ "Unknown deletion error" ],
-                    });
-                })
-                .catch(( oError ) => {
-                    oResponse.status(500).json({
-                        "errors":[oError],
-                    });
-                });
-
-        })
-        .catch(( oError ) => {
-            oResponse.status(500).json({
-                "errors":[oError],
-            })
-        })
+                    } );
+                } )
+                .catch( ( oError ) => {
+                    oResponse.status( 500 ).json( {
+                        "errors": [ oError.toString() ],
+                    } );
+                } );
+        } )
+        .catch( ( oError ) => {
+            oResponse.status( 500 ).json( {
+                "errors": [ oError.toString() ],
+            } );
+        } );
 }
