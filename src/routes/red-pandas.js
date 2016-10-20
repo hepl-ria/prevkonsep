a1a66e2c94b0b94ed77dfcd91cbc90cddb249cbd
Anne Remacle <anne.m.remacle@gmail.com>
2016-10-20 16:39:18 +0200

    route to add a panda

--- src/routes/red-pandas.js
+++ src/routes/red-pandas.js
@@ -8,7 +8,7 @@
 
 import { Router } from "express";
 import list from "../controllers/red-pandas/list";
-//import add from "../controllers/red-pandas/add";
+import add from "../controllers/red-pandas/add";
 //import details from "../controllers/red-pandas/details";
 //import destroy from "../controllers/red-pandas/destroy";
 //import update from "../controllers/red-pandas/update";
@@ -16,8 +16,8 @@ import list from "../controllers/red-pandas/list";
 let oRouter = new Router();
 
 oRouter.get( "/red-pandas", list );
-//oRouter.get( "/red-pandas/:slug", details );
-//oRouter.post( "/red-pandas", add );
+oRouter.get( "/red-pandas/:slug", details );
+oRouter.post( "/red-pandas", add );
 //oRouter.put( "/red-pandas/:slug", update );
 //oRouter.delete( "/red-pandas/:slug", destroy );
 
