9a7d821424467d1adf8edd5601bffdb7c03146c2
Anne Remacle <anne.m.remacle@gmail.com>
2016-10-20 16:24:15 +0200

    Controler to list red pandas

---
+++ src/controllers/red-pandas/list.js
@@ -0,0 +1,24 @@
+/* prevkonsep
+ *
+ * /src/controllers/red-pandas/list.js - API controller for red-pandas listing
+ *
+ * coded by Anne
+ * started at 7/10/2016
+ */
+
+import { db } from "../../core/mongodb";
+
+export default function( oRequest, oResponse ) {
+    db.collection( "red-pandas" )
+        .find( {} )
+        /* find retourne ce qu'on appelle un curseur */
+        .toArray()
+            .then( ( aCats = [] ) => {
+                oResponse.json( aPandas );
+            } )
+            .catch( ( oError ) => {
+                oResponse.status( 500 ).json( {
+                    "errors": [ oError.toString() ],
+                } );
+            } );
+}
